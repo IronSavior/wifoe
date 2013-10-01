fa36ae8b2d83753b469fd0c4a697c79df6057358
Erik Elmore <erik@erikelmore.com>
2013-10-01 02:44:35 -0400

    clarified  the socket reader code a bit

--- src/socket_reader.cpp
+++ src/socket_reader.cpp
@@ -18,16 +18,14 @@ using boost::signals2::connection;
 connection socket_reader::watch_socket( const sys::socket_handle sock, event_signal::slot_type slot ) {
   auto sig_pair = signals.find(sock);
   if( sig_pair == signals.end() ) {
-    sig_pair = signals.insert( sig_pair, std::make_pair(sock, std::shared_ptr<event_signal>(new event_signal))  );
+    sig_pair = signals.insert( sig_pair, std::make_pair(sock, std::make_shared<event_signal>()) );
   }
   return sig_pair->second->connect(slot);
 }
 
 const sys::socket_list socket_reader::socket_list() const {
   sys::socket_list list;
-  for( auto pair : signals ) {
-    list.push_back(pair.first);
-  }
+  for( auto pair : signals ) list.push_back(pair.first);
   return list;
 }
 
@@ -35,10 +33,9 @@ void socket_reader::loop() {
   terminate_loop = false;
   for(;;) {
     if( terminate_loop ) break;
-    auto read_socks = sys::select_for_read(socket_list());
-    for( auto sock : read_socks ) {
-      auto buf = sys::read_sock(sock);
-      signals[sock]->operator()(buf);
+    for( auto sock : sys::select_for_read(socket_list()) ) {
+      auto& sig = *signals[sock];
+      sig(sys::read_sock(sock));
     }
   }
 }
